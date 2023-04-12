4e1e5ec8a1b79e1a229427a597f028f047e05a48
Sydney Dickson <102965341+sydneyrd@users.noreply.github.com>
2023-04-12 10:09:33 -0500

    Initial commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+## Brewed Awakenings ERD:
+![erd](./drawSQL-brewed-awakenings-export-2023-04-12.png)
---
+++ request_handler.py
@@ -0,0 +1,72 @@
+import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+
+
+
+class HandleRequests(BaseHTTPRequestHandler):
+    """Handles the requests to this server"""
+
+    def parse_url(self, path):
+        """Parse the url into the resource and id"""
+        parsed_url = urlparse(path)
+        path_params = parsed_url.path.split('/')
+        resource = path_params[1]
+
+        if parsed_url.query:
+            query = parse_qs(parsed_url.query)
+            return (resource, query)
+
+        pk = None
+        try:
+            pk = int(path_params[2])
+        except (IndexError, ValueError):
+            pass
+        return (resource, pk)
+
+    def _set_headers(self, status):
+        """Sets the status code, Content-Type and Access-Control-Allow-Origin
+        headers on the response
+
+        Args:
+            status (number): the status code to return to the front end
+        """
+        self.send_response(status)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+
+    def do_OPTIONS(self):
+        """Sets the OPTIONS headers
+        """
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods',
+                         'GET, POST, PUT, DELETE')
+        self.send_header('Access-Control-Allow-Headers',
+                         'X-Requested-With, Content-Type, Accept')
+        self.end_headers()
+
+    def do_GET(self):
+        """Handle Get requests to the server"""
+
+    def do_POST(self):
+        """Make a post request to the server"""
+
+    def do_PUT(self):
+        """Handles PUT requests to the server"""
+
+    def do_DELETE(self):
+        """Handle DELETE Requests"""
+
+
+def main():
+    """Starts the server on port 8088 using the HandleRequests class
+    """
+    host = ''
+    port = 8088
+    HTTPServer((host, port), HandleRequests).serve_forever()
+
+
+if __name__ == "__main__":
+    main()
